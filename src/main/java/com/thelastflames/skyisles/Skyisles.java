0a4816b581708135f2026986a74aabbd3d2a48f2
GiantLuigi4 <49770992+giantluigi4@users.noreply.github.com>
2020-05-28 09:21:00 -0400

    Setup workspace.

---
+++ src/main/java/com/thelastflames/skyisles/Skyisles.java
@@ -0,0 +1,42 @@
+package com.thelastflames.skyisles;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("skyisles")
+public class Skyisles {
+	
+	// Directly reference a log4j logger.
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	public Skyisles() {
+		IEventBus bus=FMLJavaModLoadingContext.get().getModEventBus();
+		bus.addListener(this::setup);
+		bus.addListener(this::enqueueIMC);
+		bus.addListener(this::processIMC);
+		bus.addListener(this::doClientStuff);
+		
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	private void setup(final FMLCommonSetupEvent event) {
+	}
+	
+	private void doClientStuff(final FMLClientSetupEvent event) {
+	}
+	
+	private void enqueueIMC(final InterModEnqueueEvent event) {
+	}
+	
+	private void processIMC(final InterModProcessEvent event) {
+	}
+}
